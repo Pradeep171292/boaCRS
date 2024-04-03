08965e05c184838eea7b64e87b3d3ac287596076
Prasad C <selvancprasad@gmail.com>
2024-04-03 13:04:38 +0530

    Student DAO integration with Service

--- src/main/java/com/boa/crs/service/StudentCRSService.java
+++ src/main/java/com/boa/crs/service/StudentCRSService.java
@@ -3,10 +3,22 @@
  */
 package com.boa.crs.service;
 
+import java.util.List;
+
+import com.boa.crs.bean.Course;
+import com.boa.crs.bean.Grade;
+import com.boa.crs.bean.Student;
+
 /**
  * @author Admin
  *
  */
 public interface StudentCRSService {
+	
+	public void addStudent(Student student);
+	public List<Student> listStudents ();
+	public Student getStudent (Integer id);
+	public List<Grade> getStudentGrades (Integer id);
+	public List<Course> getStudentCourses (Integer id);
 
 }
--- src/main/java/com/boa/crs/service/StudentCRSServiceImpl.java
+++ src/main/java/com/boa/crs/service/StudentCRSServiceImpl.java
@@ -1,5 +1,45 @@
 package com.boa.crs.service;
 
+import java.util.List;
+
+import com.boa.crs.bean.Course;
+import com.boa.crs.bean.Grade;
+import com.boa.crs.bean.Student;
+import com.boa.crs.dao.StudentCRSDAO;
+import com.boa.crs.dao.StudentCRSDAOImpl;
+
 public class StudentCRSServiceImpl implements StudentCRSService {
+
+	StudentCRSDAO dao = new StudentCRSDAOImpl();
+	@Override
+	public void addStudent(Student student) {
+		// TODO Auto-generated method stub
+		dao.addStudent(student);
+		
+	}
+
+	@Override
+	public List<Student> listStudents() {
+		// TODO Auto-generated method stub
+		return dao.listOfStudent();
+	}
+
+	@Override
+	public Student getStudent(Integer id) {
+		// TODO Auto-generated method stub
+		return dao.getStudent(id);
+	}
+
+	@Override
+	public List<Grade> getStudentGrades(Integer id) {
+		// TODO Auto-generated method stub
+		return dao.listOfGrade(id);
+	}
+
+	@Override
+	public List<Course> getStudentCourses(Integer id) {
+		// TODO Auto-generated method stub
+		return dao.listOfCourse(id);
+	}
 
 }
